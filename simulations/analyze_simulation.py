14b4a7d9b7653c3229e4d4289bd9ae9c1e243850
GERAULT Tristan <tristan.gerault@inrae.fr>
2024-09-10 13:20:41 +0200

    sub

--- simulations/analyze_simulation.py
+++ simulations/analyze_simulation.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
     #scenarios = ["Drew_1975_low"]
 
     #output_path = "outputs"
-    output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/05-06_hairless_tests"
-    #output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/01-06_ISRR 2024"
+    #output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/05-06_hairless_tests"
+    output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/01-06_ISRR 2024"
 
     analyze_data(scenarios=scenarios, outputs_dirpath=output_path,
                      on_sums=False,
                      on_performance=True,
-                     animate_raw_logs=False,
+                     animate_raw_logs=True,
                      target_properties=None
                      )
     # In the end put the system to sleep, Windows only
