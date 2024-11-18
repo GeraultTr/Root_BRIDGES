222713b4aac1c4a88b1377de43a38360d5363d20
GeraultTr <tristan.gerault@inrae.fr>
2024-11-18 07:43:47 +0100

    more realistic test regarding root_CN

--- test/test_simple_benchmark.py
+++ test/test_simple_benchmark.py
@@ -5,6 +5,7 @@ from numba import jit, njit
 from openalea.mtg.traversal import post_order2, pre_order2
 import timeit
 
+
 # FAKE PARAMETERS
 Km_Nm_root_LATS = 1e1
 Km_Nm_root_HATS = 1e-3
@@ -89,56 +90,56 @@ def strategy_0():
     """
     Like strategy 1 but without self iterating function to assess its impact.
     """
-    for _ in range(n_repeat):
+    for _ in range(n_processes):
         for func in parallel_functions:
             props[func.__name__].update({vid:func(*(props[name][vid] for name in input_names[func.__name__])) for vid in vertices})
 
 def strategy_1():
-    for _ in range(n_repeat):
+    for _ in range(n_processes):
         for func in parallel_functions:
             props[func.__name__].update({vid:func(*(props[name][vid] for name in input_names[func.__name__])) for vid in vertices})
-        fake_complex_iterator()
+    fake_complex_iterator()
 
 def strategy_2():
-    for _ in range(n_repeat):
-        numpy_props = {name: np.array(list(props[name].values())) for name in props if isinstance(props[name], dict)}
+    numpy_props = {name: np.array(list(props[name].values())) for name in props if isinstance(props[name], dict)}
+    for _ in range(n_processes):
         for func in parallel_functions:
             props[func.__name__] = func(*(numpy_props[name] for name in input_names[func.__name__]))
-        fake_complex_iterator()
-        for name in numpy_props:
-                props[name] = dict(zip(vertices, numpy_props[name]))
+    fake_complex_iterator()
+    for name in numpy_props:
+            props[name] = dict(zip(vertices, numpy_props[name]))
 
 def strategy_3():
-    for _ in range(n_repeat):
-        numpy_props = {name: np.array(list(props[name].values())) for name in props if isinstance(props[name], dict)}
+    numpy_props = {name: np.array(list(props[name].values())) for name in props if isinstance(props[name], dict)}
+    for _ in range(n_processes):
         props["phloem_exchange_surface"] = phloem_exchange_surface_numba(*(numpy_props[name] for name in input_names["phloem_exchange_surface"]))
         props["import_Nm"] = import_Nm_numba(*(numpy_props[name] for name in input_names["import_Nm"]))
-        fake_complex_iterator()
-        for name in numpy_props:
-                props[name] = dict(zip(vertices, numpy_props[name]))
+    fake_complex_iterator()
+    for name in numpy_props:
+            props[name] = dict(zip(vertices, numpy_props[name]))
 
 def strategy_4():
     """
     Twist of strategy 1 but with properties retreived from a class to see if the bottleneck might be here.
     """
-    for _ in range(n_repeat):
+    for _ in range(n_processes):
         for func in parallel_functions:
             props[func.__name__].update({vid:func(*(getattr(class_holding_properties, name)[vid] for name in input_names[func.__name__])) for vid in vertices})
-        fake_complex_iterator()
+    fake_complex_iterator()
 
 def strategy_5():
     """
     Twist of strategy 4 but with properties retreived from an "Input class" whose values are retrieved from the other class as properties at each call.
     """
-    for _ in range(n_repeat):
+    for _ in range(n_processes):
         for func in parallel_functions:
             props[func.__name__].update({vid:func(*(getattr(receiver_instance, name)[vid] for name in input_names[func.__name__])) for vid in vertices})
-        fake_complex_iterator()
+    fake_complex_iterator()
 
 
 if __name__ == "__main__":
     mtg_path = "inputs/root_1080.pckl"
-    
+
     with open(mtg_path, "rb") as f:
         g = pickle.load(f)
 
@@ -146,18 +147,18 @@ if __name__ == "__main__":
     class_holding_properties = InputClass(g=g)
     receiver_instance = ReceiverClass(g=g, input_instance=class_holding_properties)
     vertices = list(props["struct_mass"].keys())
-
-    n_repeat = 10
+    
+    n_processes = 50
     parallel_functions = [phloem_exchange_surface, import_Nm]
     input_names = {func.__name__: ins.getfullargspec(func)[0] for func in parallel_functions}
 
     execution_times = dict(
-        strat_0=timeit.timeit(strategy_0, number=1),
-        strat_1=timeit.timeit(strategy_1, number=1),
-        strat_2=timeit.timeit(strategy_2, number=1),
-        strat_3=timeit.timeit(strategy_3, number=1),
-        strat_4=timeit.timeit(strategy_4, number=1),
-        strat_5=timeit.timeit(strategy_5, number=1)
+        strat_0=timeit.timeit(strategy_0, number=10),
+        strat_1=timeit.timeit(strategy_1, number=10),
+        strat_2=timeit.timeit(strategy_2, number=10),
+        strat_3=timeit.timeit(strategy_3, number=10),
+        strat_4=timeit.timeit(strategy_4, number=10),
+        strat_5=timeit.timeit(strategy_5, number=10)
     )
     
     print(execution_times)
