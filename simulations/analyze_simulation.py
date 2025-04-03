6cc33469bed1062a26c929114c051e43460c436d
GeraultTr <tristan.gerault@inrae.fr>
2025-04-03 17:41:07 +0200

    sub

--- simulations/analyze_simulation.py
+++ simulations/analyze_simulation.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # Utility packages
 from analyze.analyze import analyze_data, test_output_range
+from log.visualize import post_compress_gltf
 
 
 if __name__ == '__main__':
@@ -12,9 +13,12 @@ if __name__ == '__main__':
     # scenarios = ["RC_ref_30D_debug"]
     # target_days = [ 5, 7, 10, 20, 30, 40, 50, 60] #, 70, 80, 90, 100]
     # target_days = np.arange(10, 61, 1)
-    # target_days = [10, 20, 30, 40, 60]
-    target_days = [40]
-    scenarios = [f"RC_ref_{day}D" for day in target_days]
+    # target_days = [10, 20, 30, 40, 50, 60]
+    target_days = [125]
+    # scenarios = [f"RC_ref_{day}D" for day in target_days]
+    scenarios = [f"RC_ref_{day}D" + "_images" for day in target_days]
+    # scenarios = [f"RC_no_hair_{day}D" for day in target_days]
+    # scenarios = [f"RC_ref_{day}D" for day in target_days] + [f"RC_no_hair_{day}D" for day in target_days]
 
     output_path = "outputs"
     #output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/05-06_hairless_tests"
@@ -22,8 +26,10 @@ if __name__ == '__main__':
 
     # test_output_range(scenarios=scenarios, outputs_dirpath="outputs", test_file_dirpath="inputs/outputs_validation_root_cynaps_V0.xlsx")
 
+    post_compress_gltf(os.path.join(output_path, scenarios[0], "root_images"))
+
     analyze_data(scenarios=scenarios, outputs_dirpath=output_path, inputs_dirpath="inputs",
-                     on_sums=True,
+                     on_sums=False,
                      on_performance=False,
                      animate_raw_logs=True,
                      target_properties=None
--- simulations/simulation_24h_stops.py
+++ simulations/simulation_24h_stops.py
@@ -65,13 +65,14 @@ def simulate_scenarios(scenarios, simulation_length=2500, echo=True, custom_pref
 if __name__ == '__main__':
     # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["Rhizodep_ref"])
     scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_ref"])
+    # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_no_hair"])
     scenario_name = list(scenarios.keys())[0]
     scenario = list(scenarios.values())[0]
 
     # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_debug"])
     # target_days = [ 5, 7, 10, 20, 30, 40, 50, 60]
     # target_days = np.arange(10, 61, 1)
-    target_days = [40]
+    target_days = [125]
     processes = []
     max_processes = mp.cpu_count()
     for day in target_days:
@@ -82,13 +83,13 @@ if __name__ == '__main__':
                     processes.remove(proc)
             time.sleep(1)
 
-        current_scenario_name = f"{str(scenario_name)}_{day}D"
+        current_scenario_name = f"{str(scenario_name)}_{day}D" + "_images"
 
         p = mp.Process(target=single_run, kwargs=dict(scenario=scenario, 
                                                       outputs_dirpath=os.path.join("outputs", current_scenario_name),
                                                       target_day=day, simulation_length=(day + 1) * 24,
                                                       echo=True,
-                                                      log_settings=Logger.light_log))
+                                                      log_settings=Logger.heavy_log))
         p.start()
         processes.append(p)
     
