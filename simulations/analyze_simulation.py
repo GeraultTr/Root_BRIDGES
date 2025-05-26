8f0bb5e2e0e0d2f4142d5ff8e460189246810d60
GeraultTr <tristan.gerault@inrae.fr>
2025-05-26 10:52:38 +0200

    sub

--- simulations/analyze_simulation.py
+++ simulations/analyze_simulation.py
@@ -21,25 +21,52 @@ if __name__ == '__main__':
     # scenarios = [f"RC_ref_{day}D" for day in target_days] + [f"RC_no_hair_{day}D" for day in target_days]
 
     # for n_scenario in ["RC_ref", "RC_ref_0.01",	"RC_ref_0.05", "RC_ref_0.5", "RC_ref_5", "RC_ref_50"]:
-    for n_scenario in ["RC_ref"]:
-        # target_days = [10, 20, 30, 40, 50]
-        target_days = [50]
-        scenarios = [f"{n_scenario}_{day}D" for day in target_days]
+    # for n_scenario in ["RC_ref"]:
+    # # for n_scenario in ["RC_ref_big_lats"]:
+    #     # target_days = [10, 20, 30, 40, 50]
+    #     target_days = [50]
+    #     target_concentration = "5.00e-01"
+    #     # target_smax = np.logspace(0, 4, 11) * 1e-9
+    #     target_smax = [5e-6]
 
-        output_path = "outputs"
-        #output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/05-06_hairless_tests"
-        # output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/01-06_ISRR 2024"
+    #     scenarios = [f"{n_scenario}_{target_concentration}_{day}D" for day in target_days]
+    #     # scenarios = [f"{n_scenario}_{target_concentration}_{target_days[0]}D_{smax:.2e}max" for smax in target_smax]
 
-        # test_output_range(scenarios=scenarios, outputs_dirpath="outputs", test_file_dirpath="inputs/outputs_validation_root_cynaps_V0.xlsx")
+    #     # output_path = "outputs"
+    #     output_path = os.path.join("outputs", "batch_fig7")
+        
+    #     #output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/05-06_hairless_tests"
+    #     # output_path = "C:/Users/tigerault/OneDrive - agroparistech.fr/Thesis/Sujet/Modelling/saved_scenarios/01-06_ISRR 2024"
 
-        # post_compress_gltf(os.path.join(output_path, scenarios[0], "root_images"))
+    #     # test_output_range(scenarios=scenarios, outputs_dirpath="outputs", test_file_dirpath="inputs/outputs_validation_root_cynaps_V0.xlsx")
 
-        analyze_data(scenarios=scenarios, outputs_dirpath=output_path, inputs_dirpath="inputs",
-                        on_sums=False,
-                        on_performance=False,
-                        animate_raw_logs=True,
-                        target_properties=None
-                        )
+    #     # post_compress_gltf(os.path.join(output_path, scenarios[0], "root_images"))
+
+    #     analyze_data(scenarios=scenarios, outputs_dirpath=output_path, inputs_dirpath="inputs",
+    #                     on_sums=False,
+    #                     on_performance=False,
+    #                     animate_raw_logs=True,
+    #                     target_properties=None
+    #                     )
+        
+    for scenario_name in ["RC_ref"]:
+        scenarios = []
+
+        output_path = os.path.join("outputs", "batch_fig7.1")
+        
+        target_days = np.arange(10, 61, 10)
+        # target_days = [50]
+        target_concentrations = np.logspace(0, 4, 5) * 5e-3
+        # target_concentrations = [5e-1]
+        
+        for day in target_days:
+            for concentration in target_concentrations:
+                analyze_data(scenarios=[f"{str(scenario_name)}_{concentration:.2e}_{day}D"], outputs_dirpath=output_path, inputs_dirpath="inputs",
+                                on_sums=True,
+                                on_performance=False,
+                                animate_raw_logs=True,
+                                target_properties=None
+                                )
         
         # In the end put the system to sleep, Windows only
         #os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
--- simulations/simulation_24h_stops.py
+++ simulations/simulation_24h_stops.py
@@ -16,7 +16,7 @@ def single_run(scenario, outputs_dirpath="outputs", simulation_length=2500, echo
 
     logger = Logger(model_instance=root_bridges, components=root_bridges.components,
                     outputs_dirpath=outputs_dirpath, 
-                    time_step_in_hours=1, logging_period_in_hours=24,
+                    time_step_in_hours=1, logging_period_in_hours=1,
                     recording_shoot=False,
                     echo=echo, **log_settings)
     
@@ -63,11 +63,18 @@ def simulate_scenarios(scenarios, simulation_length=2500, echo=True, custom_pref
 
 
 if __name__ == '__main__':
-    # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["Rhizodep_ref"])
+    # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_ref_big_lats"])
     # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_ref_0.1",	"RC_ref_0.01",	"RC_ref_0.05",	"RC_ref_0.5",	"RC_ref_5",	"RC_ref_50"])
     scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_ref"])
-    target_concentrations = np.logspace(0, 4, 61) * 5e-3
-    target_days = np.arange(10, 61, 1)
+    # target_days = np.arange(10, 61, 1)
+    target_days = np.arange(10, 61, 10)
+    # target_days = [20]
+    # target_concentrations = np.logspace(0, 4, len(target_days)) * 5e-3
+    # target_concentrations = np.logspace(0, 4, 11) * 5e-3
+    target_concentrations = np.logspace(0, 4, 5) * 5e-3
+    # target_concentrations = [5e-1]    
+    # target_smax = np.logspace(0, 4, 11) * 1e-9 # Barillot et al. 2016
+    target_smax = [5e-6]
 
     parallel = True
     # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_ref_low"])
@@ -84,26 +91,30 @@ if __name__ == '__main__':
 
         if parallel:
             processes = []
-            max_processes = mp.cpu_count()
+            max_processes = mp.cpu_count() - 1
             for day in target_days:
                 for concentration in target_concentrations:
                     scenario["parameters"]["root_bridges"]["roots"]["dissolved_mineral_N"] = 5e-7 * concentration / 1e-1
-                    print("modified concentration to : ", scenario["parameters"]["root_bridges"]["roots"]["dissolved_mineral_N"])
-                    while len(processes) == max_processes:
-                        for proc in processes:
-                            if not proc.is_alive():
-                                processes.remove(proc)
-                        time.sleep(1)
+                    # print("modified concentration to : ", scenario["parameters"]["root_bridges"]["roots"]["dissolved_mineral_N"])
+                    for smax in target_smax:
 
-                    current_scenario_name = f"{str(scenario_name)}_{concentration:.2e}_{day}D"
+                        # Main process creation part
+                        while len(processes) == max_processes:
+                            for proc in processes:
+                                if not proc.is_alive():
+                                    processes.remove(proc)
+                            time.sleep(1)
+                        scenario["parameters"]["root_bridges"]["roots"]["smax"] = smax
+                        # current_scenario_name = f"{str(scenario_name)}_{concentration:.2e}_{day}D_{smax:.2e}max"
+                        current_scenario_name = f"{str(scenario_name)}_{concentration:.2e}_{day}D"
 
-                    p = mp.Process(target=single_run, kwargs=dict(scenario=scenario, 
-                                                                outputs_dirpath=os.path.join("outputs", current_scenario_name),
-                                                                target_day=day, simulation_length=(day + static_days) * 24,
-                                                                echo=True,
-                                                                log_settings=Logger.light_log))
-                    p.start()
-                    processes.append(p)
+                        p = mp.Process(target=single_run, kwargs=dict(scenario=scenario, 
+                                                                    outputs_dirpath=os.path.join("outputs", "batch_fig7.2", current_scenario_name),
+                                                                    target_day=day, simulation_length=(day + static_days) * 24,
+                                                                    echo=True,
+                                                                    log_settings=Logger.light_log))
+                        p.start()
+                        processes.append(p)
 
         else:
             for day in target_days:
