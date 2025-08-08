557e079b3e2a4dcffdc9c437b54ec590f65c15e7
GeraultTr <tristan.gerault@inrae.fr>
2025-08-08 18:11:58 +0200

    inputs to generate CN-Wheat input MTG

--- simulations/scene/simulation.py
+++ simulations/scene/simulation.py
@@ -15,12 +15,13 @@ from openalea.fspm.utility.plot import analyze_data
 
 
 if __name__ == "__main__":
-    scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RB_ref"])
+    # scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RB_ref"])
+    scenarios = ms.from_table(file_path="inputs/Scenarios_25_08_08.xlsx", which=["CN_Wheat_init"])
     custom_output_folder = "outputs/recoupling"
 
     scene_xrange = 0.15
     scene_yrange = 0.15
-    sowing_density = 1
+    sowing_density = 250
     environment_models_number = 1
     subprocesses_number = int(max(scene_xrange * scene_yrange * sowing_density, 1)) + environment_models_number
     parallel_development = 1 # To keep room in CPUs if launching dev simulations in parallel on the machine
@@ -67,9 +68,9 @@ if __name__ == "__main__":
             play_Orchestra(scene_name=scenario_name, output_folder=custom_output_folder, plant_models=[RootBRIDGES], plant_scenarios=[scenario], 
                                 soil_model=RhizoSoil, soil_scenario=scenario,
                                 translator_path=root_bridges.__path__[0],
-                                logger_class=Logger, log_settings=Logger.light_log,
+                                logger_class=Logger, log_settings=Logger.heavy_log,
                                 scene_xrange=scene_xrange, scene_yrange=scene_yrange, sowing_density=sowing_density,
-                                time_step=3600, n_iterations=24*50)
+                                time_step=3600, n_iterations=24*80)
             
             target_folder_key = "RootBRIDGES_0"
 
